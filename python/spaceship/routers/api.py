610d52fc98dc1987dde3169ec2c89dc93b0d236e
Svitlana <svitbar1102@gmail.com>
2023-05-28 17:05:40 +0300

    Add new dependency
    Add new endpoint. Change docker image.

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -6,3 +6,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/product')
+def culc_product() -> dict:
+    import numpy as np
+
+    matrix_a = np.random.rand(10,10)
+    matrix_b = np.random.rand(10,10)
+
+    product = np.matmul(matrix_a, matrix_b)
+
+    return {
+        'matrix_a': matrix_a.tolist(),
+        'matrix_b': matrix_b.tolist(),
+        'product': product.tolist()
+    }
